5da6564519225d078ca5d88787638315d694d745
Delmiro Ribeiro <delmiroribeiro.alpha@gmail.com>
2023-08-22 19:20:18 -0300

    edu/semanaBase/exercicioBasico

---
+++ java-anatomia-classes/src/edu/semanaBase/exercicioAprovado.java
@@ -0,0 +1,16 @@
+package edu.semanaBase;
+//Exercicio basico de aprovacão de aluno,ambientacão.
+public class exercicioAprovado {
+    public static void main(String[] args) {
+        int nota = 7;
+
+            if(nota > 6 ){
+                System.out.print("Aprovado!");
+            }else if(nota == 6){
+                System.out.println("Minerva");
+            }else{
+                System.out.println("Reprovado");
+            }
+    }
+    
+}
