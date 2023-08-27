1c43560d3f9a9ca1b5150197b7d50fc57515e087
Delmiro Ribeiro <delmiroribeiro.alpha@gmail.com>
2023-08-27 19:53:17 -0300

    preparacao/desafioscodigos

--- java-anatomia-classes/src/edu/semanaBase/MinhaClasse.java
+++ java-anatomia-classes/src/edu/semanaBase/MinhaClasse.java
@@ -1,7 +1,8 @@
 public class MinhaClasse{
 
-public static void main(String[] args) {
-    System.out.print("hello world!");
-    }
-
+    public static void main(String[] args) throws Exception {
+        int numero1 = 1;
+        String numero2 = "2";
+        System.out.println(numero1+numero2); 
+      }
 }
