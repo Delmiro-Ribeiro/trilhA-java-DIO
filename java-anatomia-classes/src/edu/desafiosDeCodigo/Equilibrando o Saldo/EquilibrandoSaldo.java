23e7f919c19326d0021f5a23d3abc676a59f4099
Delmiro Ribeiro <delmiroribeiro.alpha@gmail.com>
2023-08-27 20:17:02 -0300

    organizacao

---
+++ java-anatomia-classes/src/edu/desafiosDeCodigo/Equilibrando o Saldo/EquilibrandoSaldo.java
@@ -0,0 +1,25 @@
+/*
+ * Descrição
+Para esse desafio, considere que você foi contratado por uma empresa bancária para auxiliar nas implementações e melhorias do sistema empresarial.
+Em uma análise inicial, foi identificado pela equipe financeira a necessidade de desenvolver uma solução que permita ao cliente equilibrar seu saldo bancário.
+Dessa forma, o programa deve solicitar uma entrada que representa o saldo atual do funcionário, e após, seja informado o valor de duas transações,
+sendo elas: um depósito e um saque. O programa deve atualizar o saldo com base nas transações e exibir o saldo final.
+Informação: As transações de depósito e retirada devem ser tratadas como valores positivos e negativos, respectivamente,
+para garantir que o cálculo do saldo final seja realizado corretamente.
+ */
+
+import java.util.Scanner;
+
+public class EquilibrandoSaldo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double saldoAtual = scanner.nextDouble();
+        double valorDeposito = scanner.nextDouble();
+        double valorRetirada = scanner.nextDouble();
+
+       //TODO: Calcular o saldo atualizado de acordo com a descrição deste desafio...
+
+     //TODO: Imprimir o a saída de conforme a tabela de exemplos (uma casa decimal).
+    }
+}
